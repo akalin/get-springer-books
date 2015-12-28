00787cd63f6309a07a66e036f596009b4fb7ec9f
Frederick Akalin <akalin@gmail.com>
2015-12-28 12:03:00 -0600

    Use better filename

--- get-springer-books.py
+++ get-springer-books.py
@@ -2,16 +2,21 @@ import csv
 import re
 import sys
 import urllib
+import util
 
 def main():
     with open(sys.argv[1], 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            title = row['Item Title']
+            raw_title = row['Item Title']
+            title = util.cleanup_title(raw_title)
+
             year = row['Publication Year']
-            authors = row['Authors']
-            full_title = "%s - %s (%s)" % (title, authors, year)
-            filename = full_title + ".pdf"
+
+            raw_authors = row['Authors']
+            authors = util.cleanup_authors(raw_authors)
+
+            filename = "%s - %s (%s).pdf" % (title, authors, year)
 
             book_url = row['URL']
             pdf_url = re.sub(r'book', r'content/pdf', row['URL']) + ".pdf"
--- rename-springer-books.py
+++ rename-springer-books.py
@@ -6,33 +6,22 @@ import string
 import sys
 import urllib
 import urlparse
-
-def cleanup_title(raw_title):
-    # Special case for Serge Lang's book:
-    # http://link.springer.com/book/10.1007/978-1-4612-5142-2 .
-    if raw_title == "SL\n          2(R)":
-        return "SL_2(R)"
-    return raw_title
-
-def cleanup_authors(raw_authors):
-    # Author names are just concatenated together. Try and split them
-    # up. This won't work for names that have mixed casing, though.
-    return re.sub(r'([a-z])([A-Z])', r'\1, \2', raw_authors)
+import util
 
 def main():
     with open(sys.argv[1], 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             raw_title = row['Item Title']
-            title = cleanup_title(raw_title)
+            title = util.cleanup_title(raw_title)
 
             year = row['Publication Year']
 
             raw_authors = row['Authors']
-            authors = cleanup_authors(raw_authors)
+            authors = util.cleanup_authors(raw_authors)
 
-            old_result_filename = "%s - %s (%s).pdf" % (raw_title, raw_authors, year)
-            result_filename = "%s - %s (%s).pdf" % (title, authors, year)
+            old_filename = "%s - %s (%s).pdf" % (raw_title, raw_authors, year)
+            filename = "%s - %s (%s).pdf" % (title, authors, year)
 
             book_url = row['URL']
             pdf_url = re.sub(r'book', r'content/pdf', row['URL']) + ".pdf"
@@ -41,11 +30,11 @@ def main():
             pdf_filename = t[-1]
 
             if os.path.isfile(pdf_filename):
-                print "Found %s, renaming to %s" % (pdf_filename, result_filename)
-                os.rename(pdf_filename, result_filename)
-            elif old_result_filename != result_filename and os.path.isfile(old_result_filename):
-                print "Found %s, renaming to %s" % (old_result_filename, result_filename)
-                os.rename(old_result_filename, result_filename)
+                print "Found %s, renaming to %s" % (pdf_filename, filename)
+                os.rename(pdf_filename, filename)
+            elif old_filename != filename and os.path.isfile(old_filename):
+                print "Found %s, renaming to %s" % (old_filename, filename)
+                os.rename(old_filename, filename)
 
 if __name__ == "__main__":
     main()
